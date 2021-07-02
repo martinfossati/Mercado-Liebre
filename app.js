934e906b07d9029a98690cd7ed7ae950a424b455
martinfossati <tinfossati@gmail.com>
2021-07-02 11:45:08 -0300

    Solucionando un problema en app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Servidor corriendo!!"));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo!!");
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
